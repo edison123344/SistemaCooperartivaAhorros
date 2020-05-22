0e1ad7ec20c7d0078e8e07bf0be73896f3c8cccb
wilsonconce <49213252+wilsonconce@users.noreply.github.com>
2020-05-21 19:41:47 -0500

    primer commit integrante2

--- Logica.php
+++ Logica.php
@@ -7,6 +7,11 @@ class Logica{
         $nombreIntegrante1="Angel Jadan";
         return "Integrante1 ".$nombreIntegrante1;
     }
+
+    public function metodoIntegrante2(){
+        $nombreIntegrante2="Wilson Conce";
+        return "Integrante2 ".$nombreIntegrante2;
+    }
 }
 
 $logica=new Logica;
